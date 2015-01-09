63315a316c8e7e8262ddc759faf3629222f7ef53
Samarth Mohan <smohan94@mit.edu>
2015-01-08 21:09:33 -0500

    Parser should be functioning properly now.

--- defaultbot/util/packet_parse.py
+++ defaultbot/util/packet_parse.py
@@ -41,9 +41,9 @@ def parse_getaction(data): #<<<<<< Most important Packet.
 	index_board = 2 + parsed_dict['num_boardcards']
 	
 	#parsing stack sizes for each player 
-	parsed_dict['stack_size'] = [data.split()[index_board + 1], data.split()[index_board + 2], data.split()[index_board + 3]]
+	parsed_dict['stack_size'] = [int(data.split()[index_board + 1]), int(data.split()[index_board + 2]), int(data.split()[index_board + 3])]
 	index_stackSize = index_board + 3
-	parsed_dict['num_active_players'] = data.split()[index_stackSize + 1]
+	parsed_dict['num_active_players'] = int(data.split()[index_stackSize + 1])
 	parsed_dict['active_players'] = [data.split()[index_stackSize + 2], data.split()[index_stackSize + 3], data.split()[index_stackSize + 4]]
 	parsed_dict['num_last_action'] = int(data.split()[index_stackSize + 5])
 	
@@ -72,8 +72,9 @@ def parse_getaction(data): #<<<<<< Most important Packet.
 def parse_handover(data):
 	parsed_dict = {}
 	parsed_dict['packet_name'] = data.split()[0]
-	parsed_dict['stack_size'] = [data.split()[1], data.split()[2], data.split()[3]]
-	parsed_dict['num_boardcards'] = data.split()[4]
+	parsed_dict['stack_size'] = [int(data.split()[1]), int(data.split()[2]), int(data.split()[3])]
+	
+	parsed_dict['num_boardcards'] = int(data.split()[4])
 	boardcards = []
 	count = 1
 	while count <= parsed_dict['num_boardcards']:
@@ -81,13 +82,15 @@ def parse_handover(data):
 		count += 1
 	parsed_dict['boardcards'] = boardcards
 	index_board = 4 + parsed_dict['num_boardcards']
-	parsed_dict['num_last_action'] = data.split()[index_board + 1]
+	
+	parsed_dict['num_last_action'] = int(data.split()[index_board + 1])
 	last_action_list = []
 	count = 1
 	while count <= parsed_dict['num_last_action']:
 		last_action_list.append(data.split()[count + index_board + 1])
 		count += 1
 	parsed_dict['last_action'] = last_action_list
+	
 	parsed_dict['timeBank'] = float(data.split()[len(data.split()) - 1])
 	return parsed_dict
 
