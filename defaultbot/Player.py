607cc990199839b6a8bcc99a6dae70c31c38c4a0
Sean Hua <shua@Seans-MacBook-Air.local>
2015-01-17 15:47:26 -0500

    minor changes

--- defaultbot/Player.py
+++ defaultbot/Player.py
@@ -12,17 +12,35 @@ necessary to connect with the engine and then always returns the same action.
 It is meant as an example of how a pokerbot should communicate with the engine.
 """
 
+# Actions
+RAISE = "RAISE"
+BET = "BET"
+FOLD = "FOLD"
+
 class Player:
+
+    def __init__ (self):
+        self.opponent_1_name = ""
+        self.opponent_2_name = ""
+        self.hasPlayed_opponent_1 = False
+        self.hasPlayed_opponent_2 = False
+        self.my_hand = ""
+        self.my_stacksize = 0
+        self.list_of_stacksizes = []
+        self.hand_id = 0
+        self.num_active_players = 0
+        self.list_of_active_players = []
+        self.history_storage = {}
 
     def makeBet(action, amount, maxBet, minBet, socket):
 
-        if action == "RAISE" or action == "BET":
+        if action == RAISE or action == BET:
             playerBet = 0
             
-            if action == "RAISE":
+            if action == RAISE:
                 playerBet = min(maxBet, max(minBet, amount))
 
-            elif action == "BET":
+            elif action == BET:
                 if amount >= minBet and amount <= maxBet:
                     playerBet = amount
             
@@ -37,17 +55,7 @@ class Player:
         # Using this ensures that you get exactly one packet per read.
         f_in = input_socket.makefile()
 
-        opponent_1_name = ""
-        opponent_2_name = ""
-        hasPlayed_opponent_1 = False
-        hasPlayed_opponent_2 = False
-        my_hand = ""
-        my_stacksize = 0
-        list_of_stacksizes = []
-        hand_id = 0
-        num_active_players = 0
-        list_of_active_players = []
-        history_storage = {}
+
 
         while True:
             # Block until the engine sends us a packet.
@@ -71,32 +79,32 @@ class Player:
             print data
             
             if received_packet['packet_name'] == "KEYVALUE":
-                history_storage[received_packet['key']] = received_packet['value']
+                self.history_storage[received_packet['key']] = received_packet['value']
 
             if received_packet['packet_name'] == "NEWGAME":
-                opponent_1_name = received_packet['opponent_1_name']
-                opponent_2_name = received_packet['opponent_2_name']
+                self.opponent_1_name = received_packet['opponent_1_name']
+                self.opponent_2_name = received_packet['opponent_2_name']
 
                 # If we have already played the opponent before, 
                 # we load their statistics from the previous encounter
                 # for this game. NOTE that Historian is just an instance of the Statistician/Historian class
                 # that is not yet created. 
-                if opponent_1_name in history_storage:
-                    hasPlayed_opponent_1 = True
+                if self.opponent_1_name in history_storage:
+                    self.hasPlayed_opponent_1 = True
                     #Historian.updateOpponentStatistics(history_storage[opponent_1_name])
 
-                if opponent_2_name in history_storage:
-                    hasPlayed_opponent_2 = True
-                    #Historian.updateOpponentStatistics(history_storage[opponent_1_name])
+                if self.opponent_2_name in history_storage:
+                    self.hasPlayed_opponent_2 = True
+                    #Historian.updateOpponentStatistics(history_storage[opponent_2_name])
 
             elif received_packet['packet_name'] == "NEWHAND":
-                my_hand = received_packet['hand']
+                self.my_hand = received_packet['hand']
                 my_seat = received_packet['seat']
-                list_of_stacksizes = received_packet['stack_size']
-                my_stacksize = list_of_stacksizes[my_seat - 1]
-                hand_id = received_packet['handID']
-                num_active_players = received_packet['num_active_players']
-                list_of_active_players = received_packet['active_players']
+                self.list_of_stacksizes = received_packet['stack_size']
+                self.my_stacksize = list_of_stacksizes[my_seat - 1]
+                self.hand_id = received_packet['handID']
+                self.num_active_players = received_packet['num_active_players']
+                self.list_of_active_players = received_packet['active_players']
 
             elif received_packet['packet_name'] == "GETACTION":
                 # hand_equity = 
@@ -104,7 +112,7 @@ class Player:
                 for action in received_packet['last_action']:
                     split_action = action.split(":")
 
-                    # if split_action[0] == "BET" or split_action[0] == "RAISE":
+                    # if split_action[0] == BET or split_action[0] == RAISE:
 
                 #Compute the minimum and maximum possible bets that we can make
                 minBet = 0
@@ -113,7 +121,7 @@ class Player:
                 for response in received_packet['legal_actions']:
                     split_action = response.split(":")
 
-                    if split_action[0] == "BET" or split_action[0] == "RAISE":
+                    if split_action[0] == BET or split_action[0] == RAISE:
                         minBet = int(split_action[1])
                         maxBet = int(split_action[2])
 
@@ -126,6 +134,7 @@ class Player:
                 # s.send("PUT samarth best\n")
                 s.send("FINISH\n")
         # Clean up the socket.
+
         s.close()
 
 if __name__ == '__main__':
